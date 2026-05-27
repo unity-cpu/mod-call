5bc4b86d7f8e1e7a177030c8a42a0b867357ccde
unity-cpu <biggercreator22@gmail.com>
2026-05-27 09:27:18 -0500

    Update unitydih.js

--- api/unitydih.js
+++ api/unitydih.js
@@ -30,7 +30,7 @@ const WHITELIST = {
   "CF17CC675112D85A": "LBAAZ.",          // lazybeans
   "CEF3083A3BE0F883": "*",          // techno
   "2A4D748DEE715B68": "*", //FLOWERY
-  "7F5D7550CC93FFE6": "*", //GUINEA
+  "7F5D7550CC93FFE6": "LBAAK.", "LBAAZ.", //GUINEA
 };
 
 // Returns which flags a player is allowed to have.
