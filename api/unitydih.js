951a28b7b2ce1430d3023c9a0d412af82e99987e
unity-cpu <biggercreator22@gmail.com>
2026-06-04 10:09:35 -0500

    Update comment for bunny entry in unitydih.js

--- api/unitydih.js
+++ api/unitydih.js
@@ -14,7 +14,7 @@ const WHITELIST = {
   "DD84C718E8AFD777": "LBAAK.",          // sot
   "59FE193D73752516": "LBAAK.",          // hasser
   "56BAE470B62F4CDD": "*",          // notagirl
-  "71469BA4796CD3E4": "*",       // bunny my daddy :3
+  "71469BA4796CD3E4": "*",       // bunny
   "6BA57D0913FA0FD7": "LBAAK.",          // ᴿᵉˢᵖᵉᶜᵗsandman
   "B5346D0CA3982424": "LBAAK.",          // guinea
   "4F5C99FA420D8B74": ["LBAAK.", "LBAAZ."],          // table
