53fdd9e285cc562dcfe08dc840a6a4a2e5286c23
unity-cpu <biggercreator22@gmail.com>
2026-06-11 13:55:11 -0500

    Update unitydih.js

--- api/unitydih.js
+++ api/unitydih.js
@@ -22,7 +22,7 @@ const WHITELIST = {
   "9F3619E3FB5953E0": "LBAAZ.",          // zenngt
   "DC493DEB24FDD9B7": "LBAAZ.",          // AydenVR
   "8804634281761F0":  ["LBAAK.", "LBAAZ."],          // cazz
-  "CDAD910551C5B3C5": "LBAAZ.",          // cl0udz
+  "CDAD910551C5B3C5": ["LBAAK.", "LBAAZ."],          // cl0udz
   "BB75C720D543C50C": ["LBAAK.", "LBAAZ."],          // jaxjr
   "EA12FC6A4F8AF723": "LBAAK.",          // princess
   "2A4D748DEE715B68": "LBAAK.",          // flowery boi
