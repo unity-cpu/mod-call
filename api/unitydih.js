a7e17d105fe22d480e12f5d1762c422ae2db7793
unity-cpu <biggercreator22@gmail.com>
2026-06-19 21:08:42 -0500

    whitelist: add LBAAK. for 7F1D5737D28D4C97 (crow)

--- api/unitydih.js
+++ api/unitydih.js
@@ -69,7 +69,7 @@ const WHITELIST = {
   "71469BA4796CD3E4": ["LBAAK.", "LBAAZ.", "FORESTGUIDE."],  // bunny
   "C7FA6FECFEAE36F0": ["MMSHIRT.", "LBAAZ."],  // frog
   "A8750682ABA4DDD1": "LBAAZ.",  // Flivz
-  "7F1D5737D28D4C97": "MMSHIRT.",  // crow [CC]
+  "7F1D5737D28D4C97": ["MMSHIRT.", "LBAAK."],  // crow
 };
 
 // Content creator whitelist: playerId -> name
