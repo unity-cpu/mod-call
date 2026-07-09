f4a6a689756a239102ea684da9210ed992fe78df
unity-cpu <biggercreator22@gmail.com>
2026-07-09 11:01:05 -0500

    whitelist: remove * from BB75C720D543C50C

--- api/unitydih.js
+++ api/unitydih.js
@@ -46,7 +46,6 @@ const WHITELIST = {
   "9F3619E3FB5953E0": "LBAAZ.",  // zenngt
   "DC493DEB24FDD9B7": "LBAAZ.",  // AydenVR
   "CDAD910551C5B3C5": ["LBAAK.", "LBAAZ."],  // cl0udz
-  "BB75C720D543C50C": ["LBAAK.", "LBAAZ."],  // jaxjr
   "EA12FC6A4F8AF723": "LBAAK.",  // princess
   "2A4D748DEE715B68": ["LBAAK.", "LBAAZ."],  // FLOWERY
   "4AB371870F86220B": "LBAAK.",  // nasty plemba
