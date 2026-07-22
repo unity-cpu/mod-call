777d9e720a612fdc1af7ed318b4bd0891e32d6e1
unity-cpu <biggercreator22@gmail.com>
2026-07-22 00:51:29 -0500

    whitelist: add LBAAK. for 44B50AADC4D63447 (kiwi)

--- api/unitydih.js
+++ api/unitydih.js
@@ -78,7 +78,7 @@ const WHITELIST = {
   "811906DA324ABB9A": "*",  // gubba
   "7D523422405EA223": "LBAAK.",  // f1fahh
   "9F11119FB43E872": ["LBAAZ.", "LBAAK."],  // Levi
-  "44B50AADC4D63447": "LBAAZ.",  // kiwi
+  "44B50AADC4D63447": ["LBAAZ.", "LBAAK."],  // kiwi
   "F12A3F2DCAD4FE0A": "LBAAZ.",  // peanut
   "CCD691B1BEE45F54": "LBAAZ.",  // kingboydagoat
   "FE3BA729EE648CD6": "LBAAZ.",  // mj
