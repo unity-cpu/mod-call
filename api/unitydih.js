3fd68a265b4aeb98f326874001870943a225954c
unity-cpu <biggercreator22@gmail.com>
2026-07-23 20:17:59 -0500

    whitelist: add LBAAK. for 71469BA4796CD3E4 (bunny)

--- api/unitydih.js
+++ api/unitydih.js
@@ -49,6 +49,7 @@ const WHITELIST = {
   "35764A5E18580CF": ["LBAAK.", "LBAAZ."],  // cat
   "A323BD691D53346D": ["LBAAK.", "LBAAZ."],  // velo
   "5433C00BD5343624": "LBAAK.",  // jax
+  "71469BA4796CD3E4": "LBAAK.",  // bunny
 };
 
 // Content creator whitelist: playerId -> name
