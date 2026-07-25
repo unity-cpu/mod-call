c6481e6949ea5489d704195e17148497174d874c
unity-cpu <biggercreator22@gmail.com>
2026-07-24 20:38:26 -0500

    whitelist: add LBAAZ. for 5E3EA98EEC47883D (yojedyo)

--- api/unitydih.js
+++ api/unitydih.js
@@ -65,7 +65,7 @@ const WHITELIST = {
   "A48D3ADAFB2EBC7C": "LBAAZ.",  // AKUMA
   "84998BA1CD9AE1D6": "FORESTGUIDE.",  // dream
   "8AB439F43386ABDE": "LBAAK.",  // enzo
-  "5E3EA98EEC47883D": "FORESTGUIDE.",  // yojedyo
+  "5E3EA98EEC47883D": ["FORESTGUIDE.", "LBAAZ."],  // yojedyo
 };
 
 // Content creator whitelist: playerId -> name
