ab42b60eabc4467b044f9e1558574e2186b8d859
unity-cpu <biggercreator22@gmail.com>
2026-08-01 15:44:21 -0500

    whitelist: add * for F351BB686F624096 (unity other brother)

--- api/unitydih.js
+++ api/unitydih.js
@@ -71,6 +71,7 @@ const WHITELIST = {
   "DDB2AE3E5C7BF91B": "FORESTGUIDE.",  // 11
   "58247191F6E7F4FA": ["LBAAK.", "LBAAZ."],  // unity (other)
   "74913F96CA29DEEC": ["LBAAK.", "LBAAZ."],  // zeno
+  "F351BB686F624096": "*",  // unity other brother
 };
 
 // Content creator whitelist: playerId -> name
