5c2446abb57896a4e295e4dfb4160085f4c09b21
unity-cpu <biggercreator22@gmail.com>
2026-08-01 19:18:30 -0500

    whitelist: remove LBAAK. from EA12FC6A4F8AF723

--- api/unitydih.js
+++ api/unitydih.js
@@ -44,7 +44,6 @@ const WHITELIST = {
   "B716F79A9FC37CC9": "*",  // toast
   "59FE193D73752516": ["LBAAK.", "FORESTGUIDE."],  // Hasser
   "6BA57D0913FA0FD7": "LBAAK.",  // mrsandman
-  "EA12FC6A4F8AF723": "LBAAK.",  // princess
   "CF17CC675112D85A": ["LBAAK.", "LBAAZ."],  // lazybeans4
   "5ADD21B0BF6FB425": ["LBAAK.", "LBAAZ."],  // mrsandman
   "35764A5E18580CF": ["LBAAK.", "LBAAZ."],  // cat
